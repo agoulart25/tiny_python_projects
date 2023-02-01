1e7b4efb85ae070ed0e1f49a70e4b65d23099853
agoulart25 <acgoulart25@gmail.com>
2023-02-01 18:24:20 -0500

    upload changes in hello01 project

---
+++ 01_hello/hello.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Author : agoulart <agoulart@localhost>
+Date   : 2023-01-30
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-n',
+                        '--name',
+                        help='name to greet',
+                        metavar='name',
+                        type=str,
+                        default='World')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    person = args.name
+
+    print(f'Hello, {person}!')
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
